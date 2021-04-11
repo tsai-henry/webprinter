23d5a3bb3fbd8024c7f73c0c846dd3df60a5a2df
Henry <awesomehry@gmail.com>
2021-04-10 18:27:46 -0700

    Completed libcurl tutorial

--- main.c
+++ main.c
@@ -6,14 +6,18 @@
    resource at that URL to standard output, in the following way:
     - If the resource is text, print one line of that text per second, preceded 
     by line numbers, then exit.
+
     - If the resource is not text, print 16 bytes of the resource per line, in 
     hexadecimal with a space between each byte, preceded by file offset in hex, 
     one line per second (similar to the output of "od -t x1" on Linux)
+
     - While the lines are printing, if the user presses the "+" key, speed up 
     the display. If he presses "-", slow it down. If he presses the space bar, 
     stop the display, and restart it when he presses the space bar again. Do not
     echo these key presses to the output.
+
     - The program must respond immediately to key presses while printing.
+
     - Do not use threads. Otherwise,you may use whatever libraries are available
     and useful on your system, but bonus points using fewer libraries.
     */
@@ -23,8 +27,26 @@
 #include <string.h> 
 #include <curl/curl.h>
 
-int main(){
-// 1. get url input
-    printf("Hello World!");
+int main(int argc, char** argv){
+    CURL *curl;
+    FILE *file;
+    int result;
+
+    fp = fopen(argv[2], "wb");
+
+    curl = curl_easy_init();
+
+    curl_easy_setopt(curl, CURLOPT_URL, argv[1]);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
+    curl_easy_setopt(curl, CURLOPT_FAILONERROR, 1L);
+    result = curl_easy_perform(curl);
+    
+    if (result == CURLE_OK)
+        std::cout("Download successful!");
+    else
+        std::cout(curl_easy_strerror);
+
+    fclose(fp);
+    curl_easy_cleanup(curl);
 }
 
